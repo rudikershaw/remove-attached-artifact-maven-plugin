3185e252a4449d8e570fbd700d5582da7c80b20f
Rudi Kershaw <alexander_kershaw@hotmail.co.uk>
2023-03-02 21:33:21 +0000

    Reduce pom.xml dependencies to essentials only.

--- src/test/java/com/rudikershaw/remove/attached/artifact/RemoveAttachedArtifactsMojoTest.java
+++ src/test/java/com/rudikershaw/remove/attached/artifact/RemoveAttachedArtifactsMojoTest.java
@@ -10,33 +10,9 @@ import static org.junit.Assert.assertTrue;
 
 public class RemoveAttachedArtifactsMojoTest
 {
-    @Rule
-    public MojoRule rule = new MojoRule() {
-        @Override
-        protected void before() throws Throwable {
-
-        }
-
-        @Override
-        protected void after() {
-
-        }
-    };
-
-    /**
-     * @throws Exception if any
-     */
     @Test
     public void testSomething() throws Exception {
 
     }
-
-    /** Do not need the MojoRule. */
-    @WithoutMojo
-    @Test
-    public void testSomethingWhichDoesNotNeedTheMojoAndProbablyShouldBeExtractedIntoANewClassOfItsOwn() {
-        assertTrue( true );
-    }
-
 }
 
