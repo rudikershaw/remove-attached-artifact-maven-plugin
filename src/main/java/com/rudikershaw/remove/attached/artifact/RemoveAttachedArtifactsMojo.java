9c918678ef7636be5a10a1e0e049762198c008f0
Rudi Kershaw <alexander_kershaw@hotmail.co.uk>
2023-03-03 12:18:54 +0000

    Remove artifact files by default.

--- src/main/java/com/rudikershaw/remove/attached/artifact/RemoveAttachedArtifactsMojo.java
+++ src/main/java/com/rudikershaw/remove/attached/artifact/RemoveAttachedArtifactsMojo.java
@@ -8,6 +8,8 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
@@ -21,6 +23,9 @@ public class RemoveAttachedArtifactsMojo extends AbstractMojo {
     @Parameter
     Set<String> artifactIds;
 
+    @Parameter(defaultValue = "true")
+    boolean deleteFile = true;
+
     @Parameter(defaultValue = "${project}", readonly = true, required = true)
     MavenProject project;
 
@@ -29,6 +34,14 @@ public class RemoveAttachedArtifactsMojo extends AbstractMojo {
         for (final String artifactId : artifactIds) {
             final Optional<Artifact> artifact = artifacts.stream().filter(a -> a.getId().equals(artifactId)).findAny();
             artifact.ifPresent(artifacts::remove);
+
+            if (deleteFile && artifact.isPresent()) {
+                try {
+                    Files.delete(artifact.get().getFile().toPath());
+                } catch (IOException e) {
+                    throw new MojoExecutionException("Failed to delete artifact with ID " + artifactId, e);
+                }
+            }
         }
     }
 }
--- src/test/java/com/rudikershaw/remove/attached/artifact/RemoveAttachedArtifactsMojoTest.java
+++ src/test/java/com/rudikershaw/remove/attached/artifact/RemoveAttachedArtifactsMojoTest.java
@@ -3,15 +3,29 @@ package com.rudikershaw.remove.attached.artifact;
 
 import com.google.common.collect.Sets;
 import org.apache.maven.artifact.Artifact;
+import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
 import org.junit.Assert;
 import org.mockito.Mockito;
 import org.junit.Test;
 
+import java.io.File;
+
 public class RemoveAttachedArtifactsMojoTest
 {
     @Test
     public void testArtifactRemovedWhenItShouldBe() throws Exception {
+        RemoveAttachedArtifactsMojo mojo = new RemoveAttachedArtifactsMojo();
+        mojo.artifactIds = Sets.newHashSet("remove-me");
+        mojo.project = createTestMavenProject("remove-me");
+        mojo.deleteFile = false;
+
+        mojo.execute();
+        Assert.assertTrue(mojo.project.getAttachedArtifacts().isEmpty());
+    }
+
+    @Test(expected = MojoExecutionException.class)
+    public void testQuitsBuildIfArtifactFileCannotBeDeleted() throws Exception {
         RemoveAttachedArtifactsMojo mojo = new RemoveAttachedArtifactsMojo();
         mojo.artifactIds = Sets.newHashSet("remove-me");
         mojo.project = createTestMavenProject("remove-me");
@@ -25,6 +39,7 @@ public class RemoveAttachedArtifactsMojoTest
         RemoveAttachedArtifactsMojo mojo = new RemoveAttachedArtifactsMojo();
         mojo.artifactIds = Sets.newHashSet("remove-me");
         mojo.project = createTestMavenProject("dont-remove-me");
+        mojo.deleteFile = false;
 
         mojo.execute();
         Assert.assertFalse(mojo.project.getAttachedArtifacts().isEmpty());
@@ -34,6 +49,7 @@ public class RemoveAttachedArtifactsMojoTest
         final MavenProject mp = new MavenProject();
         final Artifact artifact = Mockito.mock(Artifact.class);
         Mockito.when(artifact.getId()).thenReturn(artifactId);
+        Mockito.when(artifact.getFile()).thenReturn(new File("/some/file/that/doesnt/exist.jar"));
         mp.addAttachedArtifact(artifact);
         return mp;
     }
